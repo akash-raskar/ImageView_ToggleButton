07f0b36c04cfcd7fde52aad4bbd24e17f3d2adf3
Akash Raskar <36689438+raskarakash2013@users.noreply.github.com>
2020-01-10 00:11:46 +0530

    Initial commit for Image View and Toggle View

---
+++ app/src/main/java/com/example/imageviewtogglebutton/Category.java
@@ -0,0 +1,21 @@
+package com.example.imageviewtogglebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+public class Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+
+    }
+
+}
